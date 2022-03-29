9797710611c5aee08835a3d030f4fe217cfe26c2
MateusFS99 <mateusfsantos.pfs@gmail.com>
2022-03-29 16:51:48 -0300

    Initial commit

---
+++ src/main/java/br/mfs/LojaVirtualApplication.java
@@ -0,0 +1,13 @@
+package br.mfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/mfs/LojaVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package br.mfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
